1bb4264ebc3d49a53198cb091f6c959c3b8b7a0c
Super-User <root@myMealz.zones.eait.uq.edu.au>
2018-03-16 11:13:22 +0000

    server ready. Awaiting proper project files

---
+++ myMealz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '597d0bced7373270b7f1e50992ee2d8153464b4c1c65d73f2433e90d0f6680bca66f73366224cec869bbc635f051c2702b70c79171baef483425959fbbf45a19'
