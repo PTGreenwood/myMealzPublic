ed5ffca45a5f70d672d5ba51fd2138ce3daada7b
PTGreenwood <peter.greenwood92@live.com.au>
2018-05-21 22:37:57 +1000

    Fixing precompile issues

--- myMealz/config/initializers/assets.rb
+++ myMealz/config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += %w( meal_planner.css, recipe_options.css )
+Rails.application.config.assets.precompile += %w( meal_planner.css)
+Rails.application.config.assets.precompile += %w( recipe_options.css)
