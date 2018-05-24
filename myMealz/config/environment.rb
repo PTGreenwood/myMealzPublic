be464a4fa37f3e719c5a94c7df5376c074c7f33c
PTGreenwood <peter.greenwood92@live.com.au>
2018-05-24 13:07:39 +1000

    Fixed boot compile issue. Probably broke everything else + icon is now in place

--- myMealz/config/environment.rb
+++ myMealz/config/environment.rb
@@ -3,11 +3,10 @@
 require File.expand_path('../application', __FILE__)
 
 # this allows WEBrick to handle pipe symbols in query parameters
-URI::DEFAULT_PARSER =
-    URI::Parser.new(:UNRESERVED => URI::REGEXP::PATTERN::UNRESERVED + '|')
+#URI::DEFAULT_PARSER = URI::Parser.new(:UNRESERVED => URI::REGEXP::PATTERN::UNRESERVED + '|')
 
-require 'pdfkit'
-config.middleware.use PDFKit::Middleware
+#require 'pdfkit'
+#config.middleware.use PDFKit::Middleware
 
 # Initialize the rails application
 MYMEALZ::Application.initialize!
